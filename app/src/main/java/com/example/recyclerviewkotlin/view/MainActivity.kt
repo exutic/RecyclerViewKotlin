0c47882fe8d094c7cf2944c3e95681af1c94d228
Arian Goodarzi <aryan.goodarzi@yahoo.com>
2021-04-27 03:37:14 +0330

    the third commit ... finished second layout, and handled setOnCLick listeners for transisioning between activities...

--- app/src/main/java/com/example/recyclerviewkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/view/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.recyclerviewkotlin.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.DatePicker
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,7 +15,7 @@ import kotlin.random.Random
 class MainActivity :
     AppCompatActivity(),
     ExampleAdapter.OnItemClickListener {
-    private val exampleList = generateDummyList(25)
+    private val exampleList = generateSampleList(5)
     private val adapter = ExampleAdapter(exampleList, this)
     private lateinit var recyclerView: RecyclerView
 
@@ -30,7 +30,7 @@ class MainActivity :
         recyclerView.setHasFixedSize(true)
     }
 
-    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
+    private fun generateSampleList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
 
         for (i in 0 until size) {
@@ -45,7 +45,7 @@ class MainActivity :
         return list
     }
 
-    fun insertItem(view: View) {
+    fun addItem(view: View) {
         val index = Random.nextInt(6)
 
         val newItem = ExampleItem(
@@ -60,17 +60,16 @@ class MainActivity :
         adapter.notifyItemInserted(index)
     }
 
-    fun removeItem(view: View) {
+    fun deleteItem(view: View) {
         val index: Int = Random.nextInt(6)
 
-        exampleList.removeAt(index)
-
         adapter.notifyItemRemoved(index)
+        Thread.sleep(500)
     }
 
     override fun onItemClick(position: Int) {
         Toast.makeText(this, "Item $position Clicked", Toast.LENGTH_SHORT).show()
-        val clickedItem : ExampleItem = exampleList[position]
+        val clickedItem: ExampleItem = exampleList[position]
         clickedItem.title = "Aryan Goodarzi"
         clickedItem.description = "Best Gamer In the world"
         clickedItem.date = "Just Now"
@@ -78,5 +77,14 @@ class MainActivity :
         adapter.notifyItemChanged(position)
 
         //in here we can go to in another page too
+
+        val intent = Intent(this, SecondActivity::class.java)
+// To pass any data to next activity
+        intent.putExtra("NewItemTitle", clickedItem.title)
+        intent.putExtra("NewItemDescription", clickedItem.description)
+        intent.putExtra("NewItemData", clickedItem.date)
+// start your next activity
+        startActivity(intent)
     }
 }
+
---
+++ app/src/main/java/com/example/recyclerviewkotlin/view/WebActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewkotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclerviewkotlin.R
+
+class WebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
